017532020b40125fb547730a3057829c2b0ca180
aram1l7 <arammartirosyann16@gmail.com>
2022-10-22 19:00:27 +0400

    fix log

--- db/redis.js
+++ db/redis.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const redis = createClient({
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
 });
-redis.on("error", (err) => console.log("Redis Client Error", err));
+redis.on("error", (err) => console.log("Redis error", err));
 
 const connectRedis = async () => {
   try {
